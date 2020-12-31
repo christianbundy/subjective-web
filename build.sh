d0d484f1589786c6565bba76225228a75fc413f5
Christian Bundy <christianbundy@fraction.io>
2020-12-31 13:23:59 -0800

    Add submodules to build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 git worktree add build
 cd build || exit 1
 git reset --hard main
+git submodule update --init --recursive --depth=1
 pandoc -s --metadata title="" -i README.md -o index.html
 rm -f README.md build.sh
 git add -A
